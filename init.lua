5cc48782cd173b2d84dacedefb014d41368f19f0
InvolvedTeacher <involvedteacher.dev@gmail.com>
2026-03-04 23:38:57 +0100

    Add neo-tree and style code

--- init.lua
+++ init.lua
@@ -1,85 +1,136 @@
--- # General Settings
--- --
+-- General Settings
 
--- Set leader to <space>
+    -- Set leader to <space>
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- ## Appearance
--- -- --
+    -- Appearance
 
--- Tab settings (tabs to 4 spaces)
+        -- Tab settings (tabs to 4 spaces)
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Vertical rulers
+        -- Vertical rulers
 vim.opt.colorcolumn = {"80", "100"}
 
--- Line numbers
+        -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
--- Autocomplete
-vim.cmd('set completeopt+=noselect,menuone,popup')
+        -- Autocomplete
+vim.cmd("set completeopt+=noselect,menuone,popup")
 
--- Other
+        -- Other appearance settings
 vim.opt.termguicolors = true
 vim.o.cursorline = true
 vim.opt.wrap = true
-vim.opt.winborder = 'rounded'
+vim.opt.winborder = "rounded"
 vim.opt.scrolloff = 10
 
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+            -- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
 
--- \\ Appearance 
+            -- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Tabs navigation
-vim.keymap.set({'n','i'}, '<C-h>', ':tabprevious<CR>')
-vim.keymap.set({'n','i'}, '<C-l>', ':tabnext<CR>')
+    -- Navigation
 
--- \\ Navigation
+        -- Tabs navigation
+vim.keymap.set({'n','i'}, "<C-h>", ":tabprevious<CR>")
+vim.keymap.set({'n','i'}, "<C-l>", ":tabnext<CR>")
 
--- Reload this file (for quick iterations trying confs)
-vim.keymap.set('n', '<leader>+', ':update<CR> :source<CR>')
+        -- Exit terminal with <esc>
+vim.keymap.set('t', "<esc>", "<c-\\><c-n>", { desc = "esc in terinal mode." })
 
--- Exit terminal with <esc>
-vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = 'esc in terinal mode.' })
+    -- Reload this file (for quick iterations trying confs)
+vim.keymap.set('n', "<leader>+", ":update<CR> :source<CR>")
 
--- Other
+    -- MAN
+vim.g.man_default_sects = '2,3'
+
+        -- Open MAN on cursor
+vim.keymap.set('n', "mm", function()
+    local cursor = vim.fn.expand('<cword>')
+    vim.api.nvim_command(':tab Man ' .. cursor)
+end, {desc='open man on cursor'})
+
+    -- Other general settings
 vim.opt.swapfile = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.mouse = "a"
-vim.g.vim_ui_open_cmd = 'gio open'
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = 'a'
+vim.g.vim_ui_open_cmd = "gio open"
 
--- \ General settings
+-- Plugin installation and setup
 
--- # Plugin installation and setup
--- --
 vim.pack.add({
-    { src = 'https://github.com/catppuccin/nvim.git' },
+    { src = "https://github.com/catppuccin/nvim.git" },
+    { src = "https://github.com/MunifTanjim/nui.nvim.git" },    -- for neo-tree
+    { src = "https://github.com/nvim-lua/plenary.nvim.git" },   -- for neo-tree
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- for neo-tree
+    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim", version = vim.version.range('3') },
 })
 
--- Catpuccin
-catppuccin = require('catppuccin')
+    -- Catpuccin
+catppuccin = require("catppuccin")
 catppuccin.setup()
-vim.cmd.colorscheme('catppuccin')
-vim.cmd.highlight({'Normal', 'guibg=none'})
+vim.cmd.colorscheme("catppuccin")
+vim.cmd.highlight({"Normal", "guibg=none"})
 vim.cmd.highlight({"NonText", "guibg=none"})
 vim.cmd.highlight({"Normal", "ctermbg=none"})
 vim.cmd.highlight({"NonText", "ctermbg=none"})
 
--- \ Plugin installation and setup 
+    -- Neo-tree
+neo_tree = require("neo-tree")
+neo_tree.setup({
+    enable_git_status = true,
+    enable_diagnostics = true,
+    event_handlers = {
+        {
+            event = "file_opened",
+            handler = function()
+                require("neo-tree.command").execute({ action = "close" })
+            end,
+        },
+    },
+    default_component_configs = {
+        git_status = {
+            symbols = {
+                -- Change type
+                added = "+", -- or "✚"
+                modified = "",
+                deleted = "-", -- this can only be used in the git_status source
+                renamed = "󰁕", -- this can only be used in the git_status source
+                -- Status type
+                untracked = "",
+                ignored = "",
+                unstaged = "󰄱",
+                staged = "",
+                conflict = "",
+            },
+        },
+    }
+})
+
+
+-- Additional keymaps
+
+    -- Open filesystem in Neo-tree
+vim.keymap.set('n', "<leader>t", function()
+    require("neo-tree.command").execute({ 
+        action="focus", source="filesystem", position="float", toggle=true 
+    })
+    end , { desc = "Filesystem tree" })
+
 
 --[[ TODO look these ones up and set up to my preferences
-vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = 'vim.lsp.buf.definition' })
-vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format' })
+vim.keymap.set('n', "<leader>gd", vim.lsp.buf.definition, { desc = "vim.lsp.buf.definition" })
+vim.keymap.set('n', "<leader>fo", vim.lsp.buf.format, { desc = "vim.lsp.buf.format" })
 ]]
 
